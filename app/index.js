765075acd2171dacc5d89ae7874208c2afd6fcf4
K3lseytaylor <mintmochi777@gmail.com>
2017-07-10 11:42:43 -0700

    Import intro example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,20 @@
-console.log('App working: open app/index.js to start exploring ES6');
+//import add from './add';
+//import { add ,subtract ,mult ,divide} from './math';5
+import * as math from './math';
+import $ from 'jquery';
+// const num1 =5;
+// const num2 =10;
+// console.log(`The total of ${num1} and ${num2} is ${add(num1 ,num2)}`);
+const {x ,y , z} = math;
+// const x = 10;
+// const y = 2 ;
+// const z = 0;
+// export default 'apples';
+console.log(math.add(x,y));
+console.log(math.subtract(x,y));
+console.log(math.mult(x,y));
+console.log(math.divide(x,y));
+console.log(math.divide(x,z));
+
+
+$('#root').append('<h1>I am helping</h1>');
---
+++ app/math.js
@@ -0,0 +1,25 @@
+export function add(x ,y){
+	return x + y;
+}
+
+export function subtract(x ,y){
+	return x - y;
+}
+
+export function mult(x ,y){
+	return x * y;
+}
+
+export function divide(x ,y){
+	if(y===0){
+		return "can't divide zero";
+	}
+	return x / y;
+}
+
+export const x = 10;
+export const y = 2 ;
+export const z = 0 ;
+apples = 'Apples';
+export default apples;
+
