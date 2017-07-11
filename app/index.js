13156df7fb538cbe8dcc035932a09561267c7a74
K3lseytaylor <mintmochi777@gmail.com>
2017-07-11 16:05:21 -0700

    ES6 class super example

--- app/index.js
+++ app/index.js
@@ -1,1 +1,42 @@
-console.log('App working: open app/index.js to start exploring ES6');
+	class Greet {
+		constructor(name){
+			this.name = name;
+		}
+
+		sayHello(){
+			return `hello ${this.name}`;
+		}
+
+		sayGoodBye(){
+			return `Good Bye ${this.name}`;
+		}
+	}
+
+
+class BetterGreeting extends Greet{
+	constructor(name, lastname){
+		super(name);
+		this.lastName = lastname;
+	}
+
+	alertHello(){
+		alert(`What up, ${this.name} ${this.lastName}`);
+	}
+}
+
+
+const DUDE = new Greet('yo');
+const JO = new Greet('JOJO');
+
+const ASH = new BetterGreeting('Lee' ,'sin');
+
+console.log(DUDE.sayHello());
+console.log(DUDE.sayGoodBye());
+
+console.log(JO.sayHello());
+console.log(JO.sayGoodBye());
+
+console.log(ASH.sayHello());
+console.log(ASH.sayGoodBye());
+
+ASH.alertHello();
